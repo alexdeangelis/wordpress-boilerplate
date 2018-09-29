e9361b236e0d6d5183bea0ab7b2e3a5e340d48a6
Alex De Angelis <alex@activeimage.co.uk>
2018-09-29 13:18:17 +0100

    bootstrap version update

--- theme/theme-name/functions.php
+++ theme/theme-name/functions.php
@@ -5,11 +5,13 @@ function register_menus() {
 add_action( 'init', 'register_menus' );
 
 
+//Enqueue CSS & JS
+
 function your_scripts() {
     /*Google Fonts*/
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Roboto:300,400,700' );
     /*Bootstrap 4*/
-    wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' );
     /*Font Awesome*/
     wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css' );
     /*Owl Carousel*/
@@ -19,9 +21,9 @@ function your_scripts() {
     /*My Styles*/
     wp_enqueue_style( 'styles', get_stylesheet_uri() );
     /*Bootstrap Popper JS*/
-    wp_enqueue_script('bootstrap-popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', array('jquery'), null, true);
+    wp_enqueue_script('bootstrap-popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('jquery'), null, true);
     /*Bootstrap JS*/
-    wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array('jquery'), null, true);
+    wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), null, true);
     /*Owl Carousel JS*/
     wp_enqueue_script('owl-carousel-js', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', array('jquery'), null, true);
 }
